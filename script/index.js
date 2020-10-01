390e9168b81680d59d26fdbe5af67fb722ad8d65
z8Dimon8z <dima-lysak@yandex.ru>
2020-10-01 18:48:43 +0300

    переменавал sub catalog

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@
 import { catalog } from './menu.js';
 import generateHeader from './generateHeader.js';
 import generateCatalog from './generateCatalog.js';
-import generateSubCatalog from './generateSubCatalog.js';
+import generateSubCatalog from './generateSubСatalog.js';
 import generateFooter from './generateFooter.js';
 import { loadData } from './loadData.js';
 
