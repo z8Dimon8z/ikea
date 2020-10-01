a17ccf6ab5bef6dc966d1e95b45e162e79d5a784
z8Dimon8z <dima-lysak@yandex.ru>
2020-10-01 18:41:23 +0300

    обратно изменил

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
-'use strict';
+
 
 import { catalog } from './menu.js';
 import generateHeader from './generateHeader.js';
 import generateCatalog from './generateCatalog.js';
-import generateSubCatalog from './generateSubсatalog.js';
+import generateSubCatalog from './generateSubCatalog.js';
 import generateFooter from './generateFooter.js';
 import { loadData } from './loadData.js';
 
